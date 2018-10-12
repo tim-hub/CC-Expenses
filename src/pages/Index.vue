e228796a2341c5e9328ca89c6376dca65698e7db
tim-hub <tim_w@outlook.com>
2018-10-12 19:32:05 +1300

    add a basic create and router

---
+++ src/pages/Index.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-page class="flex flex-center">
+  <q-btn
+    round
+    color="primary"
+    @click="nav_create"
+    class="fixed"
+    style="right: 18px; bottom: 18px"
+  >
+    +
+  </q-btn>
+
+  </q-page>
+
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex',
+  methods: {
+    nav_create () {
+      this.$router.push({
+        path: 'create'
+      })
+    }
+  }
+
+}
+</script>
