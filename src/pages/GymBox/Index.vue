a99c061e5b7fda6f3c3446d398a86c38e79dde74
tim-hub <tim_w@outlook.com>
2018-11-11 14:10:42 +1300

    update tge index layout

--- src/pages/GymBox/Index.vue
+++ src/pages/GymBox/Index.vue
@@ -1,18 +1,41 @@
 <template>
-  <q-page class="flex flex-center">
-    <!-- combine plank and pushups -->
-    <div class="col-md-12">
-            <q-btn class="vertical-top" color="secondary" size="xl" to="/start/pushup">
+<q-list highlight>
+  <q-list-header>Applications</q-list-header>
+  <q-item>
+    <q-item-side>
+      <q-item-tile avatar>
+        <img src="statics/boy-avatar.png">
+      </q-item-tile>
+    </q-item-side>
+    <q-item-main >
+        <q-btn class="vertical-top" color="secondary" size="xl" to="/start/pushup">
         Push-ups
       </q-btn>
+    </q-item-main>
+    <q-item-side right>
+      <q-item-tile icon="chat_bubble" color="green" />
+    </q-item-side>
+  </q-item>
+    <!-- combine plank and pushups -->
+    <div class="col-md-12">
+
     </div>
     <p> </p>
     <div class="col-md-12">
+
+    </div>
+
+  <q-item>
+    <q-item-side avatar="statics/linux-avatar.png" />
+    <q-item-main >
       <q-btn color="primary" size="xl" to="/start/plank">
         Plank
       </q-btn>
-    </div>
-  </q-page>
+    </q-item-main>
+    <!-- <q-item-side right icon="chat_bubble" /> -->
+  </q-item>
+</q-list>
+
 </template>
 
 <style>
